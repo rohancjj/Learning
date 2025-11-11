6b4d71ba6924df13921e9325c12f4bcad33aea88
rohancjj <rohan2002pankaj@gmail.com>
2025-11-11 14:41:45 +0530

    first commit

---
+++ middleware/errorhandler.js
@@ -0,0 +1,6 @@
+const errorhandler = (err,req,res,next) =>{
+    const statusCode = res.statusCode ? res.statusCode: 500
+    res.json({message: err.message,stackTrace: err.stack})
+}
+
+module.exports = errorhandler
