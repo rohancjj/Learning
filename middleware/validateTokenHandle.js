4d05c978c17b9316d5792ccf52637b56757e09c4
rohancjj <rohan2002pankaj@gmail.com>
2025-11-25 15:52:45 +0530

    implment jwt

---
+++ middleware/validateTokenHandle.js
@@ -0,0 +1,25 @@
+const asyncHandler = require('express-async-handler')
+const jwt = require('jsonwebtoken')
+
+const validateToken = asyncHandler(async(req,res,next)=>{
+    let token
+    let authHeader = req.headers.authorization
+    if(authHeader && authHeader.startsWith("Bearer")){
+        token = authHeader.split(" ")[1]
+        jwt.verify(token,process.env.ACCESS_TOKEN_SECERT,(err,decoded)=>{
+            if(err){
+                res.status(401)
+                throw new Error("user is not authorized")
+            }
+            req.user = decoded.user; // ✅ IMPORTANT
+            next();
+        })
+    }else {
+    res.status(401);
+    throw new Error("No token provided");
+  }
+
+})
+
+
+module.exports = validateToken
--- server.js
+++ server.js
@@ -2,15 +2,26 @@ const express = require('express')
 const contactRoutes = require('./routes/contactRoutes')
 const errorHandler = require('./middleware/errorhandler')
 const connectDb = require('./config/dbConnection')
+const userRoutes = require('./routes/userRoutes')
 
 const dotenv = require('dotenv').config()
+
+
 const errorhandler = require('./middleware/errorhandler')
+
+
 const app = express()
 const PORT = process.env.PORT || 5000
+
+
 app.use(express.json())
 connectDb()
+
 app.use("/api/contacts",contactRoutes)
+app.use("/api/users",userRoutes)
+
 app.use(errorhandler)
+
 
 app.listen(PORT,()=>{
     console.log(`server is running on PORT ${PORT}`);
